87f9121d65f147c432f06146a67baee97c1f8e68
Yash1485 <yashparekh140805@gmail.com>
2025-10-02 17:25:08 +0530

    Fix: URL issues

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors({
-    origin: process.env.ALLOWED_ORIGINS?.split(",") || ["http://localhost:3000", "https://stackchat-five.vercel.app"],
+    // origin: process.env.ALLOWED_ORIGINS?.split(",") || ["http://localhost:3000", "https://stackchat-five.vercel.app"],
+    origin: (origin, callback) => {
+        callback(null, origin || true);
+    },
     credentials: true // allow frontend to send the cookies
 }))
 
